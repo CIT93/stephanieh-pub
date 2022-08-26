8e26ff55ba6ab5e2f78515b7ae286e8fac96d561
Stephanie Holderfield <stephscode@gmail.com>
2022-08-26 11:08:51 -0700

    week 3 learn together fixed

--- w3/dinner.js
+++ w3/dinner.js
@@ -1,20 +1,21 @@
 showOnPage ('<b>DINNER PLANS</b>')
 
-let isTime = function (time = 1700) {
-    if (dinnerTime = time >=1700 && time <= 1900)
-    return showOnPage(`It is dinner time, ${result}`)       
-    else 
-    return showOnPage('It is not dinner time.')
-}
-let value = isTime()
 
 
-let dinnerDecision = function (amHungry, amlazy) {
-    if (amHungry && amlazy)
-    return ('order takeout')
-    else if (amHungry !== amlazy)
-    return ('cook dinner at home.') 
-    else if (amHungry(false))
-    return ('If youre not hungry, then do not eat.')
+let isTime = function (time = 1800) {
+    if (dinnerTime = time >=1700 && time <= 1900) {
+    showOnPage(dinnerDecision(false, true));      
+    } else {
+    return showOnPage('It is not dinner time.');
 }
-let result = dinnerDecision(false, false)
+}
+let dinnerDecision = function (amHungry, amLazy) {
+    if (amHungry && amLazy) {
+    return ('It is time for dinner, you should order takeout') 
+    } else if (amHungry || amLazy)
+    return ('It is time for dinner, you should cook dinner at home.') 
+    else  {
+    return ('It is dinner time, but you are not hungry, so you should not eat')
+}
+}
+isTime()
