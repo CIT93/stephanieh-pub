020e1d095000fa9c9e6572512b09bbf689020f51
Stephanie Holderfield <stephscode@gmail.com>
2022-09-19 20:30:55 -0700

    video 50 my code

--- w7/notes-app.js
+++ w7/notes-app.js
@@ -1,1 +1,12 @@
-console.log('This is from a new file')
+// DOM - Document Object Model
+// Query and Remove - this will remove first p tag found
+// const p = document.querySelector('p')
+// p.remove()
+
+//query and remove, show on console, or change text for all p tags
+const ps = document.querySelectorAll('p')
+ps.forEach(function (){
+    ps.textContent = '*****'
+    // console.log(p.textContent)
+   // p.remove()
+})
